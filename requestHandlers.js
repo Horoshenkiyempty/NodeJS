f44638ff692f927f8617236939f3cade42c18315
Aleksandr <45598303+Horoshenkiyempty@users.noreply.github.com>
2022-09-14 19:39:13 +0300

    Handler start/upload

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,7 +1,14 @@
 function start(){
     console.log("Request handler 'start' was called.");
+
+    function sleep (MilliSeconds) {
+        var StartTime = new Date().getTime();
+        while (new Date().getTime() < StartTime + MilliSeconds);
+    }
+    sleep(10000)
     return "Hello Start!"
 }
+
 
 function upload(){
     console.log("Request handler 'upload' was called.");
