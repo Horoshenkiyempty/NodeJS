51934bb9aac4609a96bc87f909c3ff7dc35d76a1
Horoshenkiyempty <45598303+Horoshenkiyempty@users.noreply.github.com>
2022-09-14 16:34:13 +0300

    TestVersion

---
+++ server.js
@@ -0,0 +1,36 @@
+/*var http = require("http");
+
+function start() {                                                       //Инициалзации модуля
+    function onRequest(request, response) {
+	    console.log("Request received."); 				                //Вывод в консоль сообщения
+	    response.writeHead(200, {"Content-Type": "text/pain"});		    //Заголовок ответа на запрос
+	    response.write("Hello World");				                    //Запрос на вывод сообщения
+	    response.end();						                            //Конец запроса
+    }
+
+    http.createServer(onRequest).listen(8888);                          //CallBack на запуск сервера
+    console.log("Server has started.");  				
+}
+    exports.start = start;                                              //Команда вызова вункции
+*/
+
+var http = require("http");
+var url = require("url");
+
+function start(pathname) {                                              //Инициалзации модуля
+    function onRequest(request, response) {
+        var pathname = url.parse (request.url).pathname;
+	        console.log("Request for" + pathname + " received.");       //Вывод в консоль сообщения
+	        response.writeHead(200, {"Content-Type": "text/pain"});		//Заголовок ответа на запрос
+
+            route(pathname);                                            //Вызов функции route
+
+	        response.write("Hello World");				                //Запрос на вывод сообщения
+	        response.end();						                        //Конец запроса
+    }
+
+    http.createServer(onRequest).listen(8888);                          //CallBack на запуск сервера
+    console.log("Server has started.");  				
+}
+    exports.start = start;                                              //Команда вызова вункции
+
