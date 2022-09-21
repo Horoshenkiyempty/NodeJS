56ab87d4ac6c8bc62e6c20ef2553c179e2422494
Aleksandr <45598303+Horoshenkiyempty@users.noreply.github.com>
2022-09-21 10:42:09 +0300

    server file

--- server.js
+++ server.js
@@ -1,19 +1,3 @@
-/*var http = require("http");
-
-function start() {                                                       //Инициалзации модуля
-    function onRequest(request, response) {
-	    console.log("Request received."); 				                //Вывод в консоль сообщения
-	    response.writeHead(200, {"Content-Type": "text/pain"});		    //Заголовок ответа на запрос
-	    response.write("Hello World");				                    //Запрос на вывод сообщения
-	    response.end();						                            //Конец запроса
-    }
-
-    http.createServer(onRequest).listen(8888);                          //CallBack на запуск сервера
-    console.log("Server has started.");  				
-}
-    exports.start = start;                                              //Команда вызова вункции
-*/
-
 var http = require("http");
 var url = require("url");
 
@@ -21,12 +5,8 @@ function start(pathname) {                                              //Ини
     function onRequest(request, response) {
         var pathname = url.parse (request.url).pathname;
 	        console.log("Request for" + pathname + " received.");       //Вывод в консоль сообщения
-	        response.writeHead(200, {"Content-Type": "text/pain"});		//Заголовок ответа на запрос
 
-            route(pathname);                                            //Вызов функции route
-
-	        response.write("Hello World");				                //Запрос на вывод сообщения
-	        response.end();						                        //Конец запроса
+            route(handle, pathname, response);                                            //Вызов функции route
     }
 
     http.createServer(onRequest).listen(8888);                          //CallBack на запуск сервера
