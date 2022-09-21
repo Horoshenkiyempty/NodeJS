619a89fbda60955dbefeb7354c2ad07a4e6c625a
Aleksandr <45598303+Horoshenkiyempty@users.noreply.github.com>
2022-09-21 13:02:55 +0300

    Router

--- route.js
+++ route.js
@@ -1,4 +1,11 @@
-function route (pathname) {
+function route(pathname) {
     console.log("About to route a request for" + pathname);
+    if (typeof hadnle[pathname] === 'function') {
+        hadnle[pathname](response);        
+    }else {
+        console.log("No request handler found for " + pathname);
+        response.writeHead(404, {"Content-Type": "text/plain"});
+        response.write("404 Not found");
+    }
 }
 exports.pathname = pathname;
